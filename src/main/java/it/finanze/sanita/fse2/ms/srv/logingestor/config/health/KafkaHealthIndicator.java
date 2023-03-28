38642659c39179eb787c8216f5eae2196720a5f5
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2023-03-28 20:25:56 +0200

    fix(Sonar) : Add interrupted exception

--- src/main/java/it/finanze/sanita/fse2/ms/srv/logingestor/config/health/KafkaHealthIndicator.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srv/logingestor/config/health/KafkaHealthIndicator.java
@@ -1,42 +1,34 @@
 package it.finanze.sanita.fse2.ms.srv.logingestor.config.health;
 
-import it.finanze.sanita.fse2.ms.srv.logingestor.config.kafka.KafkaPropertiesCFG;
-import it.finanze.sanita.fse2.ms.srv.logingestor.utility.ProfileUtility;
-
 import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-import java.util.Properties;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class KafkaHealthIndicator implements HealthIndicator {
 
 	@Autowired
-    private KafkaPropertiesCFG kafkaCFG;
-
-	@Autowired
-	private ProfileUtility profileUtility;
+    private AdminClient client;
 	
-    @Override
-    public Health health() {
-    	Properties configProperties = new Properties();
-    	configProperties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaCFG.getProducerBootstrapServers());
-    	if(!profileUtility.isDevOrDockerProfile() && !profileUtility.isTestProfile()) {
-    		configProperties.put("security.protocol", kafkaCFG.getProtocol());
-    		configProperties.put("sasl.mechanism", kafkaCFG.getMechanism());
-    		configProperties.put("sasl.jaas.config", kafkaCFG.getConfigJaas());
-    		configProperties.put("ssl.truststore.location", kafkaCFG.getTrustoreLocation());  
-    		configProperties.put("ssl.truststore.password", String.valueOf(kafkaCFG.getTrustorePassword())); 
-    	}
-        try(AdminClient adminClient = AdminClient.create(configProperties)) {
-            adminClient.listTopics().listings().get();
-            return Health.up().build();
-        } catch (Exception e) {
-            return Health.down(e).build();
-        }
-    }
+	@Override
+	public Health health() {
+		Health health = null;
+		try {
+			client.listTopics().listings().get();
+			health = Health.up().build();
+		} catch (InterruptedException e) {
+			log.warn("Interrupted!", e);
+			health = Health.down(e).build();
+			// Restore interrupted state...
+			Thread.currentThread().interrupt();
+		} catch (Exception e) {
+			health = Health.down(e).build();
+		}
+		return health;
+	}
 }
--- src/main/java/it/finanze/sanita/fse2/ms/srv/logingestor/config/kafka/KafkaPropertiesCFG.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srv/logingestor/config/kafka/KafkaPropertiesCFG.java
@@ -4,10 +4,16 @@
 package it.finanze.sanita.fse2.ms.srv.logingestor.config.kafka;
 
 import java.io.Serializable;
+import java.util.Properties;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import it.finanze.sanita.fse2.ms.srv.logingestor.utility.ProfileUtility;
 import lombok.Data;
 
 
@@ -62,4 +68,20 @@ public class KafkaPropertiesCFG implements Serializable {
 	@Value("${kafka.enablessl}")
 	private boolean enableSSL;
 	
+	@Autowired
+	private ProfileUtility profileUtility;
+	
+	@Bean
+	public AdminClient client() {
+		Properties configProperties = new Properties();
+    	configProperties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, producerBootstrapServers);
+    	if(!profileUtility.isDevOrDockerProfile() && !profileUtility.isTestProfile()) {
+    		configProperties.put("security.protocol", protocol);
+    		configProperties.put("sasl.mechanism", mechanism);
+    		configProperties.put("sasl.jaas.config", configJaas);
+    		configProperties.put("ssl.truststore.location", trustoreLocation);  
+    		configProperties.put("ssl.truststore.password", String.valueOf(trustorePassword)); 
+		}
+		return AdminClient.create(configProperties);
+	}
 }
