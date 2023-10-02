733b49e31258cc98d38eac257a74c04ca70abf37
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2023-10-02 21:52:04 +0200

    feat: Update props

--- src/main/java/it/finanze/sanita/fse2/ms/srv/logingestor/config/mongo/MongoDatabaseCFG.java
+++ src/main/java/it/finanze/sanita/fse2/ms/srv/logingestor/config/mongo/MongoDatabaseCFG.java
@@ -23,12 +23,20 @@ import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClients;
+
 @Configuration
 public class MongoDatabaseCFG {
 
     @Bean
     public MongoDatabaseFactory mongoDatabaseFactory(MongoPropertiesCFG mongoPropertiesCFG){
-        return new SimpleMongoClientDatabaseFactory(mongoPropertiesCFG.getUri());
+    	  ConnectionString connectionString = new ConnectionString(mongoPropertiesCFG.getUri());
+          MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+              .applyConnectionString(connectionString)
+              .build();
+          return new SimpleMongoClientDatabaseFactory(MongoClients.create(mongoClientSettings), mongoPropertiesCFG.getSchemaName());
     }
 
     @Bean
